80c184c7ac7b5dd0ef261def69584f182a082584
bradgerb <77023448+bradgerb@users.noreply.github.com>
2025-02-05 17:47:45 -0500

    Add button functionality

--- src/index.js
+++ src/index.js
@@ -5,12 +5,31 @@ import { menuPage } from "./menu-content.js";
 import { contactPage } from "./contact-content.js";
 
 const nav = (function () {
+
+    const resetDom = ()=>{
+        const content = document.getElementById("content");
+        while (content.hasChildNodes()){
+            content.removeChild(content.firstChild);
+        };
+    };
+
     const home = document.querySelector(".home");
-    home.addEventListener("click", homePage);
+    home.addEventListener("click", ()=>{
+        resetDom();
+        homePage();
+    });
 
     const menu = document.querySelector(".menu");
-    menu.addEventListener("click", menuPage);
+    menu.addEventListener("click", ()=>{
+        resetDom();
+        menuPage();
+    });
 
     const contact = document.querySelector(".contact");
-    contact.addEventListener("click", contactPage);
+    contact.addEventListener("click", ()=>{
+        resetDom();
+        contactPage();
+    });
+
+    homePage();
 })();
