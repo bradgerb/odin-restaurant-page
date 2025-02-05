21ad47122f2af872c19ab0fdb3f28773f5ad2838
bradgerb <77023448+bradgerb@users.noreply.github.com>
2025-02-05 16:48:55 -0500

    Add contact html to js file

---
+++ src/contact-content.js
@@ -0,0 +1,22 @@
+const contact = ()=> {
+    const contentContainer = document.getElementById("content");
+
+    const titleContent = document.createElement("div");
+    titleContent.classList.add("title", "bubble");
+    titleContent.textContent = "Contact Us";
+    contentContainer.appendChild(titleContent);
+
+    const contactOne = document.createElement("div");
+    contactOne.classList.add("bubble");
+        const contactOneName = document.createElement("div");
+        contactOneName.classList.add("underline");
+        contactOneName.textContent = "Head Chef";
+        contactOne.appendChild(contactOneName);
+
+        const contactOneInfo = document.createElement("div");
+        contactOneInfo.innerText = "Bob Smith\ntotallyreal@notfake.com\n867-5309";
+        contactOne.appendChild(contactOneInfo);
+    contentContainer.appendChild(contactOne);
+}
+
+export {contact};
